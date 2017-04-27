cb0654e51730fd8c223de5db8f5c9faa08a101b4
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-04-27 09:26:50 +0200

    ~ adding ORIGIN_HOST from techbook

--- wp-config.php
+++ wp-config.php
@@ -10,48 +10,98 @@ define('UPLOADS_DIR_NAME', 'uploads');
 define('APP_DIR', realpath(__DIR__ . '/../../' . APP_DIR_NAME));
 define('DATA_DIR', realpath(__DIR__ . '/../' . DATA_DIR_NAME));
 
-// load config that depends on the container environment
-$environment = getenv('ENVIRONMENT');
-if (false === $environment
-    || false === in_array($environment, ['development', 'testing', 'integration', 'production'])
-) {
-    $environment = 'development';
+/**
+ * Set Origin
+ *
+ * @return string
+ */
+function getOriginHost()
+{
+    $origin = getenv('WP_ORIGIN');
+
+    if (false === $origin) {
+        return 'http://192.168.33.10:41500';
+    }
+
+    if (false === IS_SSL) {
+        // if the current request is served over http
+        // but the origin host is configured as https
+        // rewrite the protocol
+        $origin = str_replace('https', 'http', $origin);
+    }
+
+    return $origin;
 }
 
-if ($environment != 'development') {
-    $_SERVER['HTTPS'] = 'on';
+define('ORIGIN_HOST', getOriginHost());
+
+/**
+ * Set Mobile Detection
+ *
+ * @return void
+ */
+function setUADevice()
+{
+    $uaDetect = new \Mobile_Detect();
+
+    if ($uaDetect->isMobile()) {
+        $device = 'mobile';
+    } else {
+        $device = 'desktop';
+    }
+    $_SERVER['HTTP_X_UA_DEVICE'] = $device;
 }
 
-$layer = getenv('WP_LAYER');
-if (false === $layer || false === in_array($layer, ['backend', 'frontend'])) {
-    $layer = 'frontend';
-}
+setUADevice();
 
-$configFile = "{$layer}-{$environment}.php";
-$configPath = APP_DIR . '/config/' . $configFile;
+/**
+ * Bootstrap Environment
+ *
+ * @return void
+ */
+function bootstrap()
+{
+    // load config that depends on the container environment
+    $environment = getenv('ENVIRONMENT');
+    if (false === $environment
+        || false === in_array($environment, ['development', 'testing', 'integration', 'production'])
+    ) {
+        $environment = 'development';
+    }
 
-if (false === file_exists($configPath)) {
-    die('no config available');
-} else {
-    $configData = require_once($configPath);
-    foreach ($configData as $key => $value) {
-        if (false === defined($key)) {
-            define($key, $value);
+    $layer = getenv('WP_LAYER');
+    if (false === $layer || false === in_array($layer, ['backend', 'frontend'])) {
+        $layer = 'frontend';
+    }
+    if ("true" === getenv('HTTPS_IS_ACTIVE')) {
+        $_SERVER['HTTPS'] = 'on';
+    }
+
+    $configFile = "{$layer}-{$environment}.php";
+    $configPath = APP_DIR . '/config/' . $configFile;
+
+    if (false === file_exists($configPath)) {
+        die('no config available');
+    } else {
+        $configData = require_once($configPath);
+        foreach ($configData as $key => $value) {
+            if (false === defined($key)) {
+                define($key, $value);
+            }
         }
     }
+
 }
 
-// load adtags config
-if (file_exists(APP_DIR . '/config/adtags.php')) {
-  define('ASSE_ADTAGS', require_once APP_DIR . '/config/adtags.php');
-}
+bootstrap();
 
 /**
  * Set up cache
  *
  * @return void
  */
-function initCache() {
+function initCache()
+{
     $memcacheAvailable = false;
     if (defined('MEMCACHED_HOST') && defined('MEMCACHED_PORT')) {
         global $memcached_servers;
@@ -80,3 +130,11 @@ function initCache() {
 initCache();
 
 $table_prefix = 'wp_';
+
+// load adtags config
+if (file_exists(APP_DIR . '/config/adtags.php')) {
+  define('ASSE_ADTAGS', require_once APP_DIR . '/config/adtags.php');
+}
+
+// Load WordPress settings
+require_once ABSPATH . 'wp-settings.php';
