28bdf51fed6b98f6c6fd759302a3d41097e2fdd5
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-06-14 11:53:37 +0200

    ~ fixup

--- db-error.php
+++ db-error.php
@@ -3,7 +3,7 @@
 $document_root    = $_SERVER['DOCUMENT_ROOT'];
 $document_error   = $document_root . '/error/error.html';
 
-if ( file_exists ( $document_root ) ) {
+if ( file_exists ( $document_error ) ) {
   include_once $document_error;
 }
 
