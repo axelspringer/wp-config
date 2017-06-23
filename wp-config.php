9c51b1fe7462f296898bc57103e7631ee98fa45b
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-06-23 17:54:48 +0200

    @1.1.19

--- wp-config.php
+++ wp-config.php
@@ -4,7 +4,7 @@ getenv('WP_ORIGIN') || exit;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 if ( ! defined( 'ASSE_XBOOKS_CONFIG' ) ) {
-  define( 'ASSE_XBOOKS_CONFIG', '1.1.18' );
+  define( 'ASSE_XBOOKS_CONFIG', '1.1.19' );
 }
 
 define( 'APP_DIR_NAME', 'app' );
