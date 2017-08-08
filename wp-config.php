eceaac913610b939c60b9786a96ab49dca7aabea
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-08-08 08:23:19 +0200

    ~ wordpress@4.8.1

--- wp-config.php
+++ wp-config.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/wp-config.inc.php';
 
 if ( ! defined( 'WP_CONFIG' ) ) {
-  define( 'WP_CONFIG', '1.5.12' );
+  define( 'WP_CONFIG', '1.5.13' );
 }
 
 if ( ! defined( 'APP_DIR_NAME' ) ) {
