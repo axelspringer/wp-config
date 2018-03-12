c6d48aaa4e9fbd58d7c668666ed2ed72f5fe04ac
Mathias Witt <mathias.witt@axelspringer.de>
2018-03-12 10:30:00 +0100

    PEA-6547: removed wp-config.inc.php, update wp-config

--- wp-config.php
+++ wp-config.php
@@ -5,8 +5,9 @@ getenv( 'WP_ORIGIN' ) || exit;
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/wp-config.inc.php';
 
+
 if ( ! defined( 'WP_CONFIG' ) ) {
-  define( 'WP_CONFIG', '1.5.13' );
+  define( 'WP_CONFIG', '3.0.0' );
 }
 
 if ( ! defined( 'APP_DIR_NAME' ) ) {
@@ -86,9 +87,15 @@ final class WPConfig {
     if ( ! $wp_environment = getenv( 'ENVIRONMENT' ) ) {
       $wp_environment = 'development';
     }
+    define("WP_ENVIRONMENT", $wp_environment);
 
     if ( ! $wp_layer = getenv( 'WP_LAYER' ) ) {
       $wp_layer = 'frontend';
+    }
+    $defaults = [];
+    // plugins config
+    if (defined( 'APP_DIR' ) && file_exists( APP_DIR . '/config/defaults.php' ) ) {
+      $defaults = require_once APP_DIR . '/config/defaults.php';
     }
 
     $wp_config_file = $wp_layer . '-' . $wp_environment . '.php';
@@ -99,6 +106,7 @@ final class WPConfig {
     }
 
     $wp_config_data = require_once( $wp_config_path );
+    $wp_config_data = array_merge($wp_config_data, $defaults);
 
     foreach( $wp_config_data as $config_key => $config_value ) {
       if ( ! defined( $config_key ) ) {
@@ -109,16 +117,6 @@ final class WPConfig {
     // https
     if ( 'true' === getenv( 'HTTPS_IS_ACTIVE' ) ) {
       $_SERVER['HTTPS'] = 'on';
-    }
-
-    // xBooks Links
-    if ( ! defined( 'ASSE_ADMIN_LINKS' ) ) {
-      define( 'ASSE_ADMIN_LINKS', WPConfigDefaults::Links[ $wp_environment ] );
-    }
-
-    // enable plugins
-    if ( ! defined( 'ENABLE_PLUGINS' ) ) {
-      define( 'ENABLE_PLUGINS', WPConfigDefaults::Plugins );
     }
 
     // adtags config
@@ -138,38 +136,32 @@ final class WPConfig {
    * @return mixded
    */
   public function init_memcached() {
-    global $memcached_servers;
+      $memcacheAvailable = false;
+      if (defined('MEMCACHED_HOST') && defined('MEMCACHED_PORT')) {
 
-    if( ! ( defined( 'MEMCACHED_HOST' ) && defined( 'MEMCACHED_PORT' ) ) ){
-      return;
-    }
+          global $memcached_servers;
+          $memcached_servers = [[MEMCACHED_HOST, MEMCACHED_PORT]];
 
-    $wp_memcached_available = false;
-    $memcached_servers = [ [ MEMCACHED_HOST, MEMCACHED_PORT ] ];
+          $memCache = new Memcached();
+          $memCache->addServer(MEMCACHED_HOST, MEMCACHED_PORT);
 
-    $memcached = new Memcached();
-    $memcached->addServer( MEMCACHED_HOST, MEMCACHED_PORT );
-    $memcached_stats  = @$memcached->getStats();
-    $memcached_server = MEMCACHED_HOST . ':' . MEMCACHED_PORT;
+          $stats = @$memCache->getStats();
+          $server = MEMCACHED_HOST . ':' . MEMCACHED_PORT;
 
-    $wp_memcached_available = (
-      is_array( $memcached_stats ) &&
-      array_key_exists( $memcached_server, $memcached_stats ) &&
-      array_key_exists( 'accepting_conns', $memcached_stats[ $memcached_server ] ) &&
-      $memcached_stats[ $memcached_server ][ 'accepting_conns' ] >= 1
-    );
+          $memcacheAvailable = (
+              is_array($stats) &&
+              array_key_exists($server, $stats) &&
+              array_key_exists('accepting_conns', $stats[$server]) &&
+              $stats[$server]['accepting_conns'] >= 1
+          );
+      }
+      define('MEMCACHE_AVAILABLE', $memcacheAvailable);
 
-    if ( ! defined( 'MEMCACHE_AVAILABLE' ) ) {
-      define( 'MEMCACHE_AVAILABLE', $wp_memcached_available );
-	}
-	
-	if (MEMCACHE_AVAILABLE === true) {
-		define('PODS_ALT_CACHE', true);
-		define('PODS_ALT_CACHE_DEBUG', false);
-		define('PODS_ALT_CACHE_TYPE', 'memcached');
-		define('PODS_ALT_CACHE_MEMCACHED_SERVER', MEMCACHED_HOST);
-		define('PODS_ALT_CACHE_MEMCACHED_PORT', MEMCACHED_PORT);
-	}
+      if (MEMCACHE_AVAILABLE === true) {
+          define('PODS_ALT_CACHE_TYPE', 'memcached');
+          define('PODS_ALT_CACHE_MEMCACHED_SERVER', MEMCACHED_HOST);
+          define('PODS_ALT_CACHE_MEMCACHED_PORT', MEMCACHED_PORT);
+      }
   }
 }
 
