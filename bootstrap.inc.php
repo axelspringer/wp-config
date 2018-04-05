5f93906bfd2bed7923f94346ac4e3d9a6f2c5872
Döll, Sebastian <sebastian.doell@spring-media.de>
2018-04-05 14:00:15 +0200

    ~ use MySQLi

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -12,7 +12,6 @@ abstract class SSM {
   // database
   const DB_CHARSET  = 'SSM_DB_CHARSET';
   const DB_HOST     = 'SSM_DB_HOST';
-  const DB_MYSQL_CLIENT_FLAGS = 'SSM_DB_MYSQL_CLIENT_FLAGS';
   const DB_NAME     = 'SSM_DB_NAME';
   const DB_PASSWORD = 'SSM_DB_PASSWORD';
   const DB_USER     = 'SSM_DB_USER';
@@ -29,6 +28,9 @@ abstract class SSM {
 
   // dev
   const DEV_MODE = 'SSM_DEV_MODE';
+
+  // MySQLi
+  const MYSQL_CLIENT_FLAGS = 'SSM_MYSQL_CLIENT_FLAGS';
 }
 
 /**
@@ -46,7 +48,6 @@ final class WPConfig {
     SSM::DB_USER          => 'wordpress', // these are default salts, so no magic
     SSM::DB_PASSWORD      => 'wordpress',
     SSM::DB_CHARSET       => 'utf8mb4',
-    SSM::DB_MYSQL_CLIENT_FLAGS => NULL,
     SSM::AUTH_KEY         => 'g$<|uOx~IO[#D${0%$SAG)sZ<8SxC&E1UE}/-d&+{n@SpwR5<cLb9/G/-H6B,;Dp',
     SSM::AUTH_SALT        => 'dfeEc[n>-{%W.[[qaAAKYnU/M^=&}w4ul^}5MDSi6c>w0(++jY:L@5NIZqB*QIaK',
     SSM::LOGGED_IN_KEY    => 'wZB/8(?{{&jJX.]+m%W>+R3@YI|zS W93 ysvh=~$glEt}b[+/?T[@:IpeYT)k[v',
@@ -54,7 +55,8 @@ final class WPConfig {
     SSM::NONCE_KEY        => 'UiK6X2+.= c%=5oH CL~jJ<<qvQ2QU%[pG:H-L|Tw*4+sr<?UG(9u^CcX#TeyR_N',
     SSM::NONCE_SALT       => 'p-V++V<N=G+^Aa1<}o|L^`+o&AKos=#`5breS(HNGTe%zAGTUxc ^W@o0Vw`%%S@',
     SSM::SECURE_AUTH_KEY  => '8%5/h+m4E%g{QYk]-~:=cq3D|74jX>r-#+`68=83}kUidA58WZA,6{HE8e{`5TbC',
-    SSM::SECURE_AUTH_SALT => 'h8eecI:x&~~;Sdk<vyYKa&oLX:sP]#Tw#bgehJ6<Hzx][/@S5War-x.WdauIv}eo'
+    SSM::SECURE_AUTH_SALT => 'h8eecI:x&~~;Sdk<vyYKa&oLX:sP]#Tw#bgehJ6<Hzx][/@S5War-x.WdauIv}eo',
+    SSM::MYSQL_CLIENT_FLAGS => NULL
   ];
 
   /**
@@ -105,7 +107,7 @@ final class WPConfig {
     $this->params[ SSM::DB_PASSWORD ] = $this->token;
 
     // enforce ssl
-    $this->params[ SSM::DB_MYSQL_CLIENT_FLAGS ] = MYSQLI_CLIENT_SSL;
+    $this->params[ SSM::MYSQL_CLIENT_FLAGS ] = MYSQLI_CLIENT_SSL;
   }
 
   /**
