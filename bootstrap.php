deb7ef8877769907f443c6e19d3379173cb84055
Döll, Sebastian <sebastian.doell@spring-media.de>
2018-04-05 14:27:49 +0200

    ~ adding bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -18,9 +18,9 @@ require_once __DIR__ . '/bootstrap.inc.php';
 
 // create new config
 $wp_config = new WPConfig(getenv( SSM::DEV_MODE ) && getenv( SSM::DEV_MODE ) === 'true');
-$wp_config->bootstrap();
 $wp_config->auth();
 $wp_config->proxy();
+$wp_config->bootstrap();
 
 // set global database table prefix
 $table_prefix = 'wp_';
